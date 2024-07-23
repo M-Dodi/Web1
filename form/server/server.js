5a022036314511494eb454442f36fc463aa8d090
Nio4124 <antoniozottola41204@gmail.com>
2024-07-23 13:58:25 +0200

    server

--- form/server/server.js
+++ form/server/server.js
@@ -9,6 +9,20 @@ app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' +
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/mansendfile', (req,res) => {
+    console.log("mi hai chiesto la form di invio file");
+    res.sendFile("sendfile.html",{root: './htdoc' });
+});
+        
+// gestione della url mansendfile
+app.post('/mansendfile', (req,res) => {
+    pass_ricevuta = req.query.password;
+    if (pass_ricevuta == "paperino")
+        res.sendFile("<html> Bravo" + req.query.email + "<br>Sono pronto a ricevere il file</html>");
+    else
+    res.send("<html> Attenzione, password errata</html>");
+    });
+
 //pagina di invio della form
 app.get('/formRegistrazione', (req,res) => {
     console.log("mi hai chiesto la form di registrazione");
